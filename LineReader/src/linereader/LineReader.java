35091cc696b06a8271e18559523f5132adad165a
MosesMXaba <mosesxaba@gmail.com>
2019-03-20 14:20:51 +0200

    Checked for inline block comments
    Added  code to accomodate more comments scenarios

--- LineReader/src/linereader/LineReader.java
+++ LineReader/src/linereader/LineReader.java
@@ -1,5 +1,6 @@
 // Program to count code lines without comments
 package linereader;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 
@@ -13,53 +14,52 @@ public class LineReader {
         int totalLines = 0;
         String line = "";
         String catchMultiComments = "";
-        try (BufferedReader br = new BufferedReader(new FileReader("Test5.txt"))) {    
-  
+        try (BufferedReader br = new BufferedReader(new FileReader("Test3.txt"))) {
+
+            //Loop through each line in the file.
             while ((line = br.readLine()) != null) {
                 String trimmedLine = line.trim();
-                
-                //Work with multiple or batch comments
-                if (trimmedLine.startsWith("/*") || catchMultiComments.startsWith("/*"))
-                {
-                    if (!catchMultiComments.endsWith("*/") && !catchMultiComments.startsWith("//")
-                            && !catchMultiComments.contains("*/"))
-                    {
-                        catchMultiComments +=trimmedLine;
+
+                //Work with multiple or block comments
+                if (trimmedLine.startsWith("/*") || catchMultiComments.startsWith("/*")) {
+
+                    if (IsNullOrEmpty(catchMultiComments)) {
+                        catchMultiComments = trimmedLine;
                     }
-                    else
-                    {
-                        //Make sure that the comment closes correctly
-                        String validCommentClosure = catchMultiComments.substring(catchMultiComments.lastIndexOf("*/") + 1);
-                        if (validCommentClosure.contains("/*") && !validCommentClosure.contains("*/"))
-                        {
+
+                    if (!catchMultiComments.endsWith("*/") && !catchMultiComments.startsWith("//") && !catchMultiComments.contains("*/")) {
+                        if (!catchMultiComments.equals(trimmedLine)) {
                             catchMultiComments += trimmedLine;
                         }
-                        else
-                        {
+                    } else {
+                        //Make sure that the comment closes correctly
+                        String validCommentClosure = catchMultiComments.substring(catchMultiComments.lastIndexOf("*/") + 1);
+                        if (validCommentClosure.contains("/*") && !validCommentClosure.contains("*/")) {
+                            catchMultiComments += trimmedLine;
+                        } else {
                             catchMultiComments = "";
                         }
                     }
                 }
-                
                 // Work with single line comments and count the code lines     
                 if (IsNullOrEmpty(catchMultiComments)) {
                     if (!trimmedLine.startsWith("//")
                             && !trimmedLine.endsWith("*/")
                             && !IsNullOrEmpty(trimmedLine)
-                            && (trimmedLine.contains(";") || trimmedLine.contains("{") 
+                            && (trimmedLine.contains(";") || trimmedLine.contains("{")
                             || trimmedLine.contains("}") || trimmedLine.contains(")"))) {
-                        
+
                         //count the lines that meet the requirements
                         totalLines++;
                     }
                 }
             }
-            
+
             // Display the message of the counted lines
             System.out.println("This file has contains " + totalLines + " lines of code");
-            
+
         } catch (Exception ex) {
-            System.out.println("readLine() failed.");
+            System.out.println("readLine() failed. File does not exist.");
         }
     }
 
